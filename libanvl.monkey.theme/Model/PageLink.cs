98a9a36caed1da3ea3da31668d8e9c3aadac53b0
Charles Willis <5862883+trippwill@users.noreply.github.com>
2021-12-30 22:00:50 +0000

    Continue component isolation

---
+++ libanvl.monkey.theme/Model/PageLink.cs
@@ -0,0 +1,7 @@
+﻿namespace libanvl.monkey.theme.Model;
+
+public record struct PageLink(string Title, string Href, string? Description);
+
+public record struct PageImage(string Src, string Alt);
+
+public record struct SiteInfo(string Title, string Description, PageImage? Logo);
