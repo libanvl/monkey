88a4654d63b773890fb1203e76db5591bac04959
Charles Willis <5862883+trippwill@users.noreply.github.com>
2021-12-30 22:22:05 +0000

    A bit of cleanup

--- libanvl.monkey/Program.cs
+++ libanvl.monkey/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<ThemeApp>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
