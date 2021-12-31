cdcba9f48514becea11a63386f21a6620a140a23
Charles Willis <5862883+trippwill@users.noreply.github.com>
2021-12-31 19:09:40 +0000

    Update publish script

--- cloudflare-pages-publish.sh
+++ cloudflare-pages-publish.sh
@@ -2,4 +2,5 @@ curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh;
 chmod +x dotnet-install.sh;
 ./dotnet-install.sh -c 6.0 -InstallDir ./dotnet6;
 ./dotnet6/dotnet --version;
-./dotnet6/dotnet publish libanvl.monkey/libanvl.monkey.csproj -c Release -o output;
+./dotnet6/dotnet tool restore
+./dotnet6/dotnet publish libanvl.monkey/libanvl.monkey.csproj -c Release -o output -p:SkipToolUpdate=true;
