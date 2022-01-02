2f338856d0014556b4980cc891b12d6f4d78090b
Charles Willis <5862883+trippwill@users.noreply.github.com>
2022-01-02 18:40:51 +0000

    Add versioning support

--- cloudflare-pages-publish.sh
+++
@@ -1,7 +0,0 @@
-curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh;
-chmod +x dotnet-install.sh;
-./dotnet-install.sh -c 6.0 -InstallDir ./dotnet6;
-./dotnet6/dotnet --version;
-./dotnet6/dotnet tool restore;
-./dotnet6/dotnet tool run webcompiler -- -r ./libanvl.monkey.theme/ -c ./libanvl.monkey.theme/webcompilerconfiguration.json;
-./dotnet6/dotnet publish libanvl.monkey/libanvl.monkey.csproj -c Release -o output -p:SkipWebCompiler=true;
